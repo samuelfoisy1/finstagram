17c6a1aaabf05e095ecd8020d795158da0c49983
Samuel Foisy <samuel@sfalabs.ca>
2022-02-10 01:20:24 +0000

    W4D1 + W4D2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
 
-end
+    has_many :finstagram_posts
+    has_many :comments
+    has_many :likes
+
+    validates :email, :username, uniqueness: true
+    validates :email, :avatar_url, :username, :password, presence: true
+  
+  end
